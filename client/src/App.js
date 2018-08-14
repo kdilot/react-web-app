9fbb6eb1a94ebafe2c2ce808a159abd02992ca65
kdilot <kdilot@gmail.com>
2018-08-13 21:13:06 -0700

    default

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { MenuBar, Intro } from 'components/common';
-import IncomeTable from 'components/income/IncomeTable';
+import IncomeTable2 from 'components/income/IncomeTable2';
+import IncomeContainer from 'components/income/IncomeContainer';
 import { Home, List } from 'pages';
 import { Layout } from 'antd';
 import styled from 'styled-components';
@@ -34,7 +35,8 @@ class App extends Component {
               <Route exact path='/' component={Intro} />
               <Route path='/home' component={Home} />
               <Route path='/list' component={List} />
-              <Route path='/income' component={IncomeTable} />
+              <Route path='/income' component={IncomeContainer} />
+              <Route path='/income2' component={IncomeTable2} />
             </Switch>
           </Content>
           <Footer style={{ textAlign: 'center' }}>
